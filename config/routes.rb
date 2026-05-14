76240fee6a41b73c80ff49db11b70f473ca31a72
Ayush Pai <ayushpai@ayushpai.com>
2026-05-14 12:28:42 +0530

    create records

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,5 @@ Rails.application.routes.draw do
   root "movies#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
+  resources :movies
 end
